e45e228c6dd303f675ce00a7f73ad8b72fb39c84
高冠祺 <307590625@qq.com>
2022-06-09 21:48:41 +0800

    Refresh

--- lib/pages/home/nav/first/controller.dart
+++ lib/pages/home/nav/first/controller.dart
@@ -1,5 +1,47 @@
 import 'package:get/get.dart';
+import 'package:ibase_app/common/utils/utils.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class FirstController extends GetxController{
 
+  final RefreshController refreshController = RefreshController();
+
+  //  允许下拉
+  final bool enablePullDown = true;
+  //  允许上拉加载
+  final bool enablePullUp = true;
+
+  List<String> list = [];
+
+
+
+  @override
+  void onReady() {
+    for (int i = 0; i < 10; i++) {
+      list.add(i.toString());
+    }
+    if(list.isNotEmpty) {
+      update(['list']);
+    }
+    super.onReady();
+  }
+
+
+
+  void onRefresh() {
+    DelayedUtils.delayed(() {
+      if(refreshController.isRefresh){
+        refreshController.refreshToIdle();
+      }
+    });
+  }
+
+  void onLoading(){
+    DelayedUtils.delayed(() {
+      if(refreshController.isLoading){
+        LogUtils.GGQ('---->onLoading');
+        refreshController.loadComplete();
+      }
+    });
+  }
 }
--- lib/pages/home/nav/first/view.dart
+++ lib/pages/home/nav/first/view.dart
@@ -1,19 +1,63 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ibase_app/common/utils/utils.dart';
+import 'package:ibase_app/common/values/values.dart';
+import 'package:ibase_app/common/widget/refresh/refresh.dart';
 import 'package:ibase_app/pages/home/nav/first/first.dart';
 
 class FirstView extends GetView<FirstController>{
 
-  const FirstView({Key? key}) : super(key: key);
+  FirstView({Key? key}) : super(key: key);
+
+
+
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.redAccent,
-      child: Center(
-        child: Text('社区'),
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+      appBar: AppBar(
+        elevation: 0.2,
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        titleTextStyle: AppStyles.titleStyle,
+        iconTheme: const IconThemeData(color: Colors.redAccent,size: 20.0),
+        backgroundColor: Colors.white,
+        title: const Text('首页'),
+        actions: [
+          IconButton(onPressed: (){
+            ToastUtils.show('scan');
+          },
+          splashRadius: 22.0,
+          icon: const Icon(Iconfont.scan))
+        ],
       ),
+      body: _buildContent(),
     );
   }
+
+  // ListView.builder(itemBuilder: (BuildContext context, int index) => _buildItem(context,index),itemCount: 5);
+  // scrollDirection: Axis.vertical,
+  // physics: NeverScrollableScrollPhysics(),
+  
+  Widget _buildContent(){
+    return GetBuilder<FirstController>(
+      id: 'list',
+      builder: (_) => controller.list.isNotEmpty? Refresh(
+        controller: controller.refreshController,
+        enablePullDown: controller.enablePullDown,
+        enablePullUp: controller.enablePullUp,
+        onRefresh: ()=> controller.onRefresh(),
+        onLoading: ()=> controller.onLoading(),
+        content: ListView.builder(
+          itemBuilder: (BuildContext context,int index) => _buildItem(context,index),itemCount: 5),
+      ): const Center(child: SizedBox(width: 28.0,height: 28.0,child: CircularProgressIndicator(strokeWidth: 2.0,color: Colors.redAccent,)),),
+    );
+  }
+
+  Widget _buildItem(BuildContext context,int index){
+    return Text(index.toString());
+  }
+
+
 
 }
